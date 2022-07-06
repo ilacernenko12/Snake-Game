62d9e5281b6044ddb4f8a5572f7c336421b1d162
Ilya Chernenko <93983025+ilacernenko12@users.noreply.github.com>
2022-07-06 16:00:09 +0300

    Create GameField.java

---
+++ GameField.java
@@ -0,0 +1,151 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.Random;
+
+public class GameField extends JPanel implements ActionListener { //то что в нем отображается
+    private final int SIZE = 320; // размер поля
+    private final int DOT_SIZE = 16; // размер клетки
+    private final int ALL_DOTS = 400; // количество клеток
+
+    // картинки
+    private Image apple;
+    private Image dot;
+
+    // координаты
+    private int appleX;
+    private int appleY;
+
+    private int [] x = new int[ALL_DOTS];
+    private int [] y = new int[ALL_DOTS];
+
+    private int dots;
+    private Timer timer; // с какой частотой мы будем обновлять экран
+    private boolean inGame = true;
+    private boolean right = true;
+    private boolean left= false;
+    private boolean up = false;
+    private boolean down = false;
+    public void createApple() { // создаем координату яблока
+        appleX = new Random().nextInt(20)*DOT_SIZE;
+        appleY = new Random().nextInt(20)*DOT_SIZE;
+    }
+
+    public void loadImage(){ // сеттер для наших картинок
+        ImageIcon iia = new ImageIcon("apple.png");
+        apple = iia.getImage();
+        ImageIcon iid = new ImageIcon("dot.png");
+        dot = iid.getImage();
+    }
+
+    public void checkApple(){
+        if (appleX==x[0] && appleY == y[0]) {
+            dots++;
+            createApple();
+        }
+    }
+
+    public void initGame(){
+        dots = 3;
+        for (int i = 0; i < dots; i++) {
+            y[i] = 3 * DOT_SIZE;
+            x[i] = DOT_SIZE*4 - i*DOT_SIZE;
+        }
+        timer = new Timer(100, this);
+        timer.start();
+        createApple();
+    }
+
+    public void checkCollision(){
+        for (int i = dots; i >3 ; i--) {
+            if (x[0]==x[i] && y[0] == y[i])
+                inGame = false;
+        }
+        if (x[0]>SIZE)
+            inGame = false;
+        if (x[0]<0)
+            inGame = false;
+        if (y[0]>SIZE)
+            inGame = false;
+        if (y[0]<0)
+            inGame = false;
+    }
+
+    public void move () {
+        for (int i = dots; i > 0; i--) {
+            x[i] = x[i-1];
+            y[i] = y[i-1];
+        }
+        if (left) {
+            x[0] -= DOT_SIZE;
+        }
+        if (right) {
+            x[0] += DOT_SIZE;
+        }
+        if (up) {
+            y[0] -= DOT_SIZE;
+        }
+        if (down) {
+            y[0] += DOT_SIZE;
+        }
+    }
+    @Override
+    protected void paintComponent (Graphics g) {
+        super.paintComponent(g);
+        if (inGame) {
+            g.drawImage(apple, appleX, appleY, this);
+            for (int i = 0; i < dots; i++) {
+                g.drawImage(dot,x[i], y[i], this);
+            }
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (inGame) {
+            checkApple();
+            checkCollision();
+            move();
+        }
+        repaint();
+    }
+
+    public GameField() {
+        setBackground(Color.BLACK);
+        loadImage();
+        initGame();
+        addKeyListener(new FieldKeyListener());
+        setFocusable(true);
+    }
+
+    class FieldKeyListener extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            super.keyPressed(e); // кладет в объект "е" ту кнопку которую мы нажали
+            int key = e.getKeyCode();
+            if (key==KeyEvent.VK_LEFT && !right){
+                left = true;
+                up = false;
+                down = false;
+            }
+            if (key==KeyEvent.VK_UP && !down) {
+                up = true;
+                left = false;
+                right = false;
+            }
+            if (key==KeyEvent.VK_DOWN && !up) {
+                down = true;
+                left = false;
+                right = false;
+            }
+            if (key==KeyEvent.VK_RIGHT && !left) {
+                right = true;
+                up = false;
+                down = false;
+            }
+        }
+    }
+}
